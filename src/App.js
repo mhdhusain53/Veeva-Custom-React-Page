20ee17f562dc7860c48524e85d29c1169b00feee
mhdhusain53 <mhdhusain53@gmail.com>
2025-07-10 15:37:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '2rem', fontSize: '1.5rem', fontFamily: 'Arial, sans-serif' }}>
+      Welcome to First React Custom Page
     </div>
   );
 }
